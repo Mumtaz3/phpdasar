6ce5f70da8f1bc026b175126f387a4b1ea090db4
yazidsupriadi <yazidsupriadi14@gmail.com>
2021-02-24 15:17:03 +0700

    belajar php dasar

---
+++ belajar.php
@@ -0,0 +1,30 @@
+<?php
+
+    define("PHI",3.14);
+    
+    $jari_jari = 7;
+
+    $luas_lingkaran = PHI * $jari_jari * $jari_jari;
+    echo $luas_lingkaran;
+
+
+    $angka = 1;
+    $nama = "yazid";
+    $nama_lengkap = 'muhammad yazid supriadi';
+    $angka_desimal = 3.5;
+    echo $angka;
+    echo "<h1>haii</h1>";
+    echo "</br>";
+    echo $nama;
+    echo "</br>";
+    echo $angka_desimal;
+    
+    echo "</br>";
+    echo $nama_lengkap;
+    
+    echo "</br>";
+    echo "dokumen saya : ".$_SERVER["DOCUMENT_ROOT"];
+    
+    echo "</br>";
+    echo "nama saya ".$nama_lengkap;
+?>
